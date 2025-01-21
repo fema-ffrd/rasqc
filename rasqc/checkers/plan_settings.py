825f5c0bd785d9aa54b6149fe643cf8b232cd48f
Jacob Bates <jhb0020@gmail.com>
2025-01-21 10:05:19 -0600

    correctly handle eqn set for multi 2d areas

--- rasqc/checkers/plan_settings.py
+++ rasqc/checkers/plan_settings.py
@@ -15,20 +15,21 @@ class EquationSet2D(RasqcChecker):
         filename = plan_hdf_path.name
         plan_hdf = RasPlanHdf(plan_hdf_path)
         plan_params = plan_hdf.get_plan_param_attrs()
-        equation_set = (
-            plan_params["2D Equation Set"]
+        equation_sets = (
+            [plan_params["2D Equation Set"]]
             if isinstance(plan_params["2D Equation Set"], str)
-            else plan_params["2D Equation Set"][0]
+            else plan_params["2D Equation Set"]
         )  # handle geometries with single or multiple 2D areas
-        if equation_set != "Diffusion Wave":
-            return RasqcResult(
-                name=self.name,
-                filename=filename,
-                result=ResultStatus.WARNING,
-                message=(
-                    f"2D Equation Set '{equation_set}'"
-                    " does not match expected setting: 'Diffusion Wave'."
-                    f" ({filename})"
-                ),
-            )
+        for equation_set in equation_sets:
+            if equation_set != "Diffusion Wave":
+                return RasqcResult(
+                    name=self.name,
+                    filename=filename,
+                    result=ResultStatus.WARNING,
+                    message=(
+                        f"2D Equation Set '{equation_set}'"
+                        " does not match expected setting: 'Diffusion Wave'."
+                        f" ({filename})"
+                    ),
+                )
         return RasqcResult(name=self.name, result=ResultStatus.OK, filename=filename)
--- tests/test_plan_settings.py
+++ tests/test_plan_settings.py
@@ -4,10 +4,18 @@ from rasqc.checkers.plan_settings import EquationSet2D
 
 TEST_DATA = Path("./tests/data")
 BALDEAGLE_PRJ = TEST_DATA / "ras/BaldEagleDamBrk.prj"
+MUNCIE_PRJ = TEST_DATA / "ras/Muncie.prj"
 
 
-def test_EquationSet2D():
+def test_EquationSet2D_a():
     assert (
         EquationSet2D().run(RasModel(BALDEAGLE_PRJ)).__dict__.__str__()
         == "{'result': <ResultStatus.OK: 'ok'>, 'name': '2D Equation Set', 'filename': 'BaldEagleDamBrk.p18.hdf', 'message': None, 'gdf': None}"
+    )
+
+
+def test_EquationSet2D_b():
+    assert (
+        EquationSet2D().run(RasModel(MUNCIE_PRJ)).__dict__.__str__()
+        == "{'result': <ResultStatus.OK: 'ok'>, 'name': '2D Equation Set', 'filename': 'Muncie.p03.hdf', 'message': None, 'gdf': None}"
     )
