28a902eb22196abc8fb82da049ebdd05677160a3
Jacob Bates <jhb0020@gmail.com>
2025-03-12 16:54:27 -0500

    add breakline enforcement check

--- rasqc/checkers/__init__.py
+++ rasqc/checkers/__init__.py
@@ -5,3 +5,4 @@ from .ras_version import *
 from .current_plan import *
 from .associated_layers import *
 from .file_structure import *
+from .breaklines import *
---
+++ rasqc/checkers/breaklines.py
@@ -0,0 +1,34 @@
+from rasqc.checkers.base_checker import RasqcChecker
+from rasqc.checksuite import register_check
+from rasqc.rasmodel import RasModel
+from rasqc.result import RasqcResult, ResultStatus
+
+from rashdf import RasGeomHdf
+
+
+@register_check(["ble"])
+class BreaklineEnforcement(RasqcChecker):
+    name = "Breakline Enforcement"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        geom_hdf_path = ras_model.current_geometry.hdf_path
+        filename = geom_hdf_path.name
+        with RasGeomHdf(geom_hdf_path) as geom_hdf:
+            mesh_faces = geom_hdf.mesh_cell_faces()
+            bls = geom_hdf.breaklines()
+            flags_all = bls.overlay(mesh_faces.buffer(1).to_frame(), how="difference", keep_geom_type=True).explode()
+            flags_filtered = flags_all[flags_all["geometry"].length >= 10]
+        if flags_filtered.empty:
+            return RasqcResult(
+                name=self.name,
+                filename=filename,
+                result=ResultStatus.OK,
+                message="No breakline enforcement flags found.",
+            )
+        return RasqcResult(
+            name=self.name,
+            filename=filename,
+            result=ResultStatus.WARNING,
+            message="No breakline enforcement flags found.",
+            gdf=flags_filtered,
+        )
