cb06db06365887ef8db68ca07ce830826f664991
Stevenray Janke <stevenrayjanke@yahoo.com>
2025-04-24 16:12:36 -0400

    Add utility functions for result outputs

---
+++ rasqc/utils.py
@@ -0,0 +1,58 @@
+"""Utility functions."""
+
+from rasqc.result import RasqcResult, ResultStatus
+from collections import defaultdict
+import pandas as pd
+import json
+
+
+def results_to_json(results: list[RasqcResult], json_fn: str) -> dict:
+    """Create a json from a list of RasqcResults."""
+
+    summary = {"passed": defaultdict(lambda: defaultdict(list)), "failed": defaultdict(lambda: defaultdict(list))}
+
+    for result in results:
+        group = "passed" if result.result == ResultStatus.OK else "failed"
+        check_name = result.name
+        filename = result.filename
+
+        if result.message and isinstance(result.message, str):
+            if result.message.startswith("'") and "':" in result.message:
+                value = result.message.split("':")[0].strip("'")
+            elif result.message.startswith("'") and result.message.endswith(
+                "does not match any of the expected patterns."
+            ):
+                value = result.message.strip("'").rsplit("'", 1)[0]
+            else:
+                value = result.message.strip()
+        else:
+            value = "N/A"
+
+        summary[group][check_name][filename].append(value)
+
+    total_results = {
+        "passed": {k: dict(v) for k, v in summary["passed"].items()},
+        "failed": {k: dict(v) for k, v in summary["failed"].items()},
+    }
+
+    with open(json_fn, "w") as f:
+        json.dump(total_results, f, indent=2)
+
+
+def write_json_results_to_excel(json_results: dict, output_path: str):
+    """Creates an excel from a json of RasqcResults. Creates 2 excel sheets for passed and failed."""
+
+    def flatten(group_name):
+        rows = []
+        for pattern, files in json_results.get(group_name, {}).items():
+            for file, props in files.items():
+                for prop in props:
+                    rows.append({"Pattern Name": pattern, "File Name": file, "RAS Property Name": prop})
+        return pd.DataFrame(rows)
+
+    passed_df = flatten("passed")
+    failed_df = flatten("failed")
+
+    with pd.ExcelWriter(output_path) as writer:
+        failed_df.to_excel(writer, sheet_name="failed", index=False)
+        passed_df.to_excel(writer, sheet_name="passed", index=False)
