897eaf4896854763464e7f18acc53bbb5cdaf490
Stevenray Janke <stevenrayjanke@yahoo.com>
2025-04-29 10:39:12 -0400

    Add constants for schema urls

---
+++ rasqc/constants.py
@@ -0,0 +1,3 @@
+RAS_SCHEMA_URL = "https://raw.githubusercontent.com/fema-ffrd/ffrd-conventions/main/hec-ras/json-schema/schema.json"
+
+HMS_SCHEMA_URL = "https://raw.githubusercontent.com/fema-ffrd/ffrd-conventions/main/hec-hms/json-schema/schema.json"
