325266cfde35a636130b1d488f35e63b65d5eec3
Jacob Bates <jhb0020@gmail.com>
2025-06-09 13:00:02 -0500

    add several event conditions checks

--- rasqc/checkers/event_conditions.py
+++ rasqc/checkers/event_conditions.py
@@ -7,7 +7,8 @@ from ..result import RasqcResult, ResultStatus, RasqcResultEncoder
 
 from json import dumps
 
-BC_PATH = "/Event Conditions/Unsteady/Boundary Conditions/"
+BC_PATH = "/Event Conditions/Unsteady/Boundary Conditions"
+IC_PATH = "/Event Conditions/Unsteady/Initial Conditions"
 
 
 @register_check(["ble"])
@@ -74,22 +75,243 @@ class PrecipHydrographs(RasqcChecker):
         """
         precip_hydrographs_path = BC_PATH + "/Precipitation Hydrographs"
         msg_dict = {}
-        attrs = ["2D Flow Area", "Start Date", "End Date"]
+        attrs = ["2D Flow Area"]
         for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
             if plan.hdf:
                 if precip_hydrographs_path in plan.hdf:
                     for p in plan.hdf[precip_hydrographs_path].values():
-                        msg_dict[plan.path.suffix.strip(".") + f" ({plan.title})"] = {
-                            k: v for k, v in p.attrs.items() if k in attrs
-                        } | dict(depth=round(float(p[:, 1].sum()), 3))
+                        msg_dict.setdefault(plan_key, []).append(
+                            {k: v for k, v in p.attrs.items() if k in attrs}
+                            | {"Depth": round(float(p[:, 1].sum()), 3)}
+                        )
                 else:
-                    msg_dict[plan.path.suffix.strip(".") + f" ({plan.title})"] = (
-                        "No precip hydrograph applied."
-                    )
+                    msg_dict[plan_key] = "No precip hydrograph applied."
             else:
-                msg_dict[plan.path.suffix.strip(".") + f" ({plan.title})"] = (
-                    "No HDF file located."
-                )
+                msg_dict[plan_key] = "No HDF file located."
+        return RasqcResult(
+            name=self.name,
+            filename=ras_model.prj_file.path.name,
+            result=ResultStatus.NOTE,
+            message=dumps(msg_dict, cls=RasqcResultEncoder),
+        )
+
+
+@register_check(["ble"])
+class FlowHydrographs(RasqcChecker):
+    """Flow hydrographs check.
+
+    Reports the boundary condition flow hydrographs applied to the HEC-RAS model. Result status is 'note'.
+    """
+
+    name = "Flow Hydrographs"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        """Execute flow hydrographs check of the HEC-RAS model.
+
+        Parameters
+        ----------
+            ras_model: RasModel
+                The HEC-RAS model to check.
+
+        Returns
+        -------
+            RasqcResult: The result of the check.
+        """
+        flow_hydrographs_path = BC_PATH + "/Flow Hydrographs"
+        msg_dict = {}
+        attrs = ["2D Flow Area", "BC Line", "EG Slope For Distributing Flow"]
+        for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
+            if plan.hdf:
+                if flow_hydrographs_path in plan.hdf:
+                    for p in plan.hdf[flow_hydrographs_path].values():
+                        msg_dict.setdefault(plan_key, []).append(
+                            {k: v for k, v in p.attrs.items() if k in attrs}
+                            | {"Peak Flow": round(float(p[:, 1].max()), 3)}
+                        )
+                else:
+                    msg_dict[plan_key] = "No flow hydrograph applied."
+            else:
+                msg_dict[plan_key] = "No HDF file located."
+        return RasqcResult(
+            name=self.name,
+            filename=ras_model.prj_file.path.name,
+            result=ResultStatus.NOTE,
+            message=dumps(msg_dict, cls=RasqcResultEncoder),
+        )
+
+
+@register_check(["ble"])
+class StageHydrographs(RasqcChecker):
+    """Stage hydrographs check.
+
+    Reports the boundary condition stage hydrographs applied to the HEC-RAS model. Result status is 'note'.
+    """
+
+    name = "Stage Hydrographs"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        """Execute stage hydrographs check of the HEC-RAS model.
+
+        Parameters
+        ----------
+            ras_model: RasModel
+                The HEC-RAS model to check.
+
+        Returns
+        -------
+            RasqcResult: The result of the check.
+        """
+        stage_hydrographs_path = BC_PATH + "/Stage Hydrographs"
+        msg_dict = {}
+        attrs = ["2D Flow Area", "BC Line", "Use Initial Stage"]
+        for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
+            if plan.hdf:
+                if stage_hydrographs_path in plan.hdf:
+                    for p in plan.hdf[stage_hydrographs_path].values():
+                        msg_dict.setdefault(plan_key, []).append(
+                            {k: v for k, v in p.attrs.items() if k in attrs}
+                            | {"Peak Stage": round(float(p[:, 1].max()), 3)}
+                        )
+                else:
+                    msg_dict[plan_key] = "No stage hydrograph applied."
+            else:
+                msg_dict[plan_key] = "No HDF file located."
+        return RasqcResult(
+            name=self.name,
+            filename=ras_model.prj_file.path.name,
+            result=ResultStatus.NOTE,
+            message=dumps(msg_dict, cls=RasqcResultEncoder),
+        )
+
+
+@register_check(["ble"])
+class NormalDepths(RasqcChecker):
+    """Normal depths check.
+
+    Reports the boundary condition normal depths applied to the HEC-RAS model. Result status is 'note'.
+    """
+
+    name = "Normal Depths"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        """Execute normal depths check of the HEC-RAS model.
+
+        Parameters
+        ----------
+            ras_model: RasModel
+                The HEC-RAS model to check.
+
+        Returns
+        -------
+            RasqcResult: The result of the check.
+        """
+        normal_depths_path = BC_PATH + "/Normal Depths"
+        msg_dict = {}
+        attrs = ["2D Flow Area", "BC Line", "BC Line WS"]
+        for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
+            if plan.hdf:
+                if normal_depths_path in plan.hdf:
+                    for p in plan.hdf[normal_depths_path].values():
+                        msg_dict.setdefault(plan_key, []).append(
+                            {k: v for k, v in p.attrs.items() if k in attrs}
+                            | {"Normal Depth Slope": float(p[0])}
+                        )
+                else:
+                    msg_dict[plan_key] = "No normal depth applied."
+            else:
+                msg_dict[plan_key] = "No HDF file located."
+        return RasqcResult(
+            name=self.name,
+            filename=ras_model.prj_file.path.name,
+            result=ResultStatus.NOTE,
+            message=dumps(msg_dict, cls=RasqcResultEncoder),
+        )
+
+
+@register_check(["ble"])
+class RatingCurves(RasqcChecker):
+    """Rating curves check.
+
+    Reports the boundary condition rating curves applied to the HEC-RAS model. Result status is 'note'.
+    """
+
+    name = "Rating Curves"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        """Execute rating curves check of the HEC-RAS model.
+
+        Parameters
+        ----------
+            ras_model: RasModel
+                The HEC-RAS model to check.
+
+        Returns
+        -------
+            RasqcResult: The result of the check.
+        """
+        rating_curves_path = BC_PATH + "/Rating Curves"
+        msg_dict = {}
+        attrs = ["2D Flow Area", "BC Line"]
+        for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
+            if plan.hdf:
+                if rating_curves_path in plan.hdf:
+                    for p in plan.hdf[rating_curves_path].values():
+                        msg_dict.setdefault(plan_key, []).append(
+                            {k: v for k, v in p.attrs.items() if k in attrs}
+                        )
+                else:
+                    msg_dict[plan_key] = "No rating curve applied."
+            else:
+                msg_dict[plan_key] = "No HDF file located."
+        return RasqcResult(
+            name=self.name,
+            filename=ras_model.prj_file.path.name,
+            result=ResultStatus.NOTE,
+            message=dumps(msg_dict, cls=RasqcResultEncoder),
+        )
+
+
+@register_check(["ble"])
+class InitialConditions(RasqcChecker):
+    """Initial conditions check.
+
+    Reports the initial conditions applied to the HEC-RAS model. Result status is 'note'.
+    """
+
+    name = "Initial Conditions"
+
+    def run(self, ras_model: RasModel) -> RasqcResult:
+        """Execute initial conditions check of the HEC-RAS model.
+
+        Parameters
+        ----------
+            ras_model: RasModel
+                The HEC-RAS model to check.
+
+        Returns
+        -------
+            RasqcResult: The result of the check.
+        """
+        ic_name_path = IC_PATH + "/IC Point Names"
+        ic_elev_path = IC_PATH + "/IC Point Elevations"
+        msg_dict = {}
+        for plan in ras_model.plan_files.values():
+            plan_key = plan.path.suffix.strip(".") + f" ({plan.title})"
+            if plan.hdf:
+                if ic_name_path in plan.hdf:
+                    msg_dict[plan_key] = {
+                        k.decode(): round(v, 3)
+                        for k, v in zip(plan.hdf[ic_name_path], plan.hdf[ic_elev_path])
+                    }
+                else:
+                    msg_dict[plan_key] = "No initial conditions applied."
+            else:
+                msg_dict[plan_key] = "No HDF file located."
         return RasqcResult(
             name=self.name,
             filename=ras_model.prj_file.path.name,
--- tests/test_event_conditions.py
+++ tests/test_event_conditions.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from rasqc.rasmodel import RasModel
-from rasqc.checkers.event_conditions import MeteorologyPrecip, PrecipHydrographs
+from rasqc.checkers.event_conditions import *
 from rasqc.result import ResultStatus
 
 TEST_DATA = Path("./tests/data")
@@ -28,6 +28,76 @@ def test_PrecipHydrographs():
         "filename": "BaldEagleDamBrk.prj",
         "element": None,
         "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": "No precip hydrograph applied."}',
+        "pattern": None,
+        "pattern_description": None,
+        "examples": None,
+        "gdf": None,
+    }
+
+
+def test_FlowHydrographs():
+    assert FlowHydrographs().run(RasModel(BALDEAGLE_PRJ)).to_dict() == {
+        "result": ResultStatus.NOTE,
+        "name": "Flow Hydrographs",
+        "filename": "BaldEagleDamBrk.prj",
+        "element": None,
+        "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": [{"2D Flow Area": "Upper 2D Area", "BC Line": "Upstream Q", "EG Slope For Distributing Flow": 0.0010000000474974513, "Peak Flow": 22000.0}]}',
+        "pattern": None,
+        "pattern_description": None,
+        "examples": None,
+        "gdf": None,
+    }
+
+
+def test_StageHydrographs():
+    assert StageHydrographs().run(RasModel(BALDEAGLE_PRJ)).to_dict() == {
+        "result": ResultStatus.NOTE,
+        "name": "Stage Hydrographs",
+        "filename": "BaldEagleDamBrk.prj",
+        "element": None,
+        "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": "No stage hydrograph applied."}',
+        "pattern": None,
+        "pattern_description": None,
+        "examples": None,
+        "gdf": None,
+    }
+
+
+def test_NormalDepths():
+    assert NormalDepths().run(RasModel(BALDEAGLE_PRJ)).to_dict() == {
+        "result": ResultStatus.NOTE,
+        "name": "Normal Depths",
+        "filename": "BaldEagleDamBrk.prj",
+        "element": None,
+        "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": [{"2D Flow Area": "BaldEagleCr", "BC Line": "DS2NormalD", "BC Line WS": "Multiple", "Normal Depth Slope": 0.0003000000142492354}, {"2D Flow Area": "BaldEagleCr", "BC Line": "DSNormalDepth", "BC Line WS": "Multiple", "Normal Depth Slope": 0.0003000000142492354}]}',
+        "pattern": None,
+        "pattern_description": None,
+        "examples": None,
+        "gdf": None,
+    }
+
+
+def test_RatingCurves():
+    assert RatingCurves().run(RasModel(BALDEAGLE_PRJ)).to_dict() == {
+        "result": ResultStatus.NOTE,
+        "name": "Rating Curves",
+        "filename": "BaldEagleDamBrk.prj",
+        "element": None,
+        "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": "No rating curve applied."}',
+        "pattern": None,
+        "pattern_description": None,
+        "examples": None,
+        "gdf": None,
+    }
+
+
+def test_InitialConditions():
+    assert InitialConditions().run(RasModel(BALDEAGLE_PRJ)).to_dict() == {
+        "result": ResultStatus.NOTE,
+        "name": "Initial Conditions",
+        "filename": "BaldEagleDamBrk.prj",
+        "element": None,
+        "message": '{"p13 (PMF with Multi 2D Areas)": "No HDF file located.", "p18 (2D to 2D Run)": "No initial conditions applied."}',
         "pattern": None,
         "pattern_description": None,
         "examples": None,
